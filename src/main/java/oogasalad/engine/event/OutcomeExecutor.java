b42a1489d9e6f1d26338a4e9a220c10fac918943
Gage Garcia <gvg7@duke.edu>
2025-03-31 16:01:39 -0400

    Feature OOGA Event-2: OutcomeExecutor Boilerplate

---
+++ src/main/java/oogasalad/engine/event/OutcomeExecutor.java
@@ -0,0 +1,29 @@
+/**
+ * Updates game state to reflect event outcome
+ * @author Gage Garcia
+ */
+package oogasalad.engine.event;
+import java.util.Map;
+import oogasalad.engine.controller.GameController;
+
+public class OutcomeExecutor {
+    private GameController gameController;
+
+    /**
+     * Initialize the executor with a game controller
+     * @param gameController
+     */
+    public OutcomeExecutor(GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    /**
+     * executes outcome using parameter map using game controller
+     * @param outcomeType
+     * @param param
+     */
+    public void executeOutcome(EventOutcome.OutcomeType outcomeType, Map<String, String> param) {
+
+    }
+
+}
