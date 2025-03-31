a7e43c323691a6d3a46b6db6d6c0581d1772367d
Jacob You <jay27@duke.edu>
2025-03-31 18:56:52 -0400

    DATA: Renamed/moved packages

---
+++ src/main/java/oogasalad/editor/view/tools/objectfactory/EntityObjectFactory.java
@@ -0,0 +1,15 @@
+package oogasalad.editor.view.tools.objectfactory;
+
+/**
+ * Factory interface for creating entity objects
+ */
+public interface EntityObjectFactory {
+  /**
+   * Creates an entity object
+   *
+   * @param x X-coordinate in pixels
+   * @param y Y-coordinate in pixels
+   * @return The created entity object
+   */
+  //EditorObject createEntityObject(double x, double y);
+}
