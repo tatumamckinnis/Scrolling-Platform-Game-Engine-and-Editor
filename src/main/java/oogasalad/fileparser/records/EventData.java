13ad5fc9c92b53854fae9d4ce5ff03c76226e38e
Billy McCune <wrm29@duke.edu>
2025-03-31 18:15:16 -0400

    OOGA-32 Refactor: changed parsing to match new data standards

--- src/main/java/oogasalad/fileparser/records/EventData.java
+++ src/main/java/oogasalad/fileparser/records/EventData.java
@@ -7,7 +7,10 @@ import java.util.List;
  * @author Billy McCune
  */
 public record EventData(
-    int order,
+    String name,
+    String type,
     String eventId,
+    List<List<String>> conditions,
+    List<String> outcomes,
     List<String> parameters
 ) {}
