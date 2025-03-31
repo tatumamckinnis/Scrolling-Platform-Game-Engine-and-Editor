ab7789af85534ae77af4b949d03df1b21e83a413
Gage Garcia <gvg7@duke.edu>
2025-03-31 15:30:06 -0400

    Feature OOGA Event-1: Event Outcome Model class

--- src/main/java/oogasalad/engine/model/event/EventOutcome.java
+++ src/main/java/oogasalad/engine/model/event/EventOutcome.java
@@ -1,4 +1,29 @@
+/**
+ * Defines valid event outcomes and associated parameters
+ * @author Gage Garcia
+ */
 package oogasalad.engine.model.event;
 
+import java.util.Map;
+
 public class EventOutcome {
+    /**
+     * Defines valid outcome types
+     */
+    public enum OutcomeType {
+        JUMP,
+        DESTROY_OBJECT
+    }
+
+    private OutcomeType outcomeType;
+
+    //defines expected parameters for each outcome type
+    private Map<OutcomeType, Map<String, String>> outcomeParamMap;
+
+    /**
+     * @return the associated parameter mapping for a specific outcome type
+     */
+    public Map<String, String> getOutcomeParamMap(OutcomeType) {
+        return outcomeParamMap.get(outcomeType);
+    }
 }
