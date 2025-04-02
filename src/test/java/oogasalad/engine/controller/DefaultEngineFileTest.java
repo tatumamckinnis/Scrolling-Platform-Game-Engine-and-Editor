64fc181994b4ca1d3a24a511cdd5e9a979b19f5d
Alana Zinkin <abz8@duke.edu>
2025-04-01 22:28:59 -0400

    Test: fix Default Engine File Test code

--- src/test/java/oogasalad/engine/controller/DefaultEngineFileTest.java
+++ src/test/java/oogasalad/engine/controller/DefaultEngineFileTest.java
@@ -1,5 +1,6 @@
 package oogasalad.engine.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -11,8 +12,10 @@ import java.util.zip.DataFormatException;
 import oogasalad.engine.model.object.DynamicVariableCollection;
 import oogasalad.engine.model.object.GameObject;
 import oogasalad.engine.model.object.Player;
+import oogasalad.fileparser.records.BlueprintData;
 import oogasalad.fileparser.records.FrameData;
 import oogasalad.fileparser.records.GameObjectData;
+import oogasalad.fileparser.records.HitBoxData;
 import oogasalad.fileparser.records.LevelData;
 import oogasalad.fileparser.records.SpriteData;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,12 +29,15 @@ class DefaultEngineFileTest {
   @BeforeEach
   void setUp() {
     myEngineFile = new DefaultEngineFileConverter();
+    Map<Integer, BlueprintData> bluePrintMap = new HashMap<>();
+    SpriteData spriteData1 = new SpriteData("Mario", new FrameData("Mario Paused", 1, 1, 2, 4, new File("dd")), new ArrayList<>(), new ArrayList<>());
+    HitBoxData hitBoxData1 = new HitBoxData("Mario", 1, 1, 2, 4);
+    bluePrintMap.put(1, new BlueprintData(1, "Mario", "Player", "Player", spriteData1, hitBoxData1, new ArrayList<>(), new HashMap<>()));
     List<GameObjectData> gameObjectBluePrintData = new ArrayList<>();
-    SpriteData spriteData1 = new SpriteData("Mario", 1, 1, 2, 4, new FrameData("Mario Paused", 1, 1, 2, 4), new ArrayList<>(), new ArrayList<>());
-    GameObjectData gameObject1 = new GameObjectData(1, new UUID(4, 1), "Mario", "Player", "Player", spriteData1, 1, 1, 1, new ArrayList<>());
+    GameObjectData gameObject1 = new GameObjectData(1, new UUID(4, 1), 1, 1, 0);
     gameObjectBluePrintData.add(gameObject1);
     Map<Integer, List<GameObjectData>> gameObjectsByLayer = new HashMap<>();
-    levelData = new LevelData("Super Mario Bros", gameObjectBluePrintData, gameObjectsByLayer);
+    levelData = new LevelData("Super Mario Bros", bluePrintMap, gameObjectsByLayer);
   }
 
   @Test
@@ -44,7 +50,7 @@ class DefaultEngineFileTest {
       throws DataFormatException, IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 
     List<GameObject> expectedObjects = new ArrayList<>();
-    SpriteData expectedSpriteData1 = new SpriteData("Mario", 1, 1, 2, 4, new FrameData("Mario Paused", 1, 1, 2, 4), new ArrayList<>(), new ArrayList<>());
+    SpriteData expectedSpriteData1 = new SpriteData("Mario", new FrameData("Mario Paused", 1, 1, 2, 4, new File("dd")), new ArrayList<>(), new ArrayList<>());
     expectedObjects.add(new Player(new UUID(4, 1), 1, 1, 1, 5, 10, 0, "Mario", "Player", expectedSpriteData1, new DynamicVariableCollection(), new ArrayList<>()));
     Map<String, GameObject> actualObjects = myEngineFile.loadFileToEngine(levelData);
     List<GameObject> myActualObjects = new ArrayList<>(actualObjects.values());
