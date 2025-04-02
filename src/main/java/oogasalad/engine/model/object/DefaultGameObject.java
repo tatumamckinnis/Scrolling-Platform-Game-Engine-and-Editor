55a2ca131cb707dd5ea2e3f509944c60bd06752c
Alana Zinkin <abz8@duke.edu>
2025-04-01 23:53:44 -0400

    Merge: resolve conflicts

--- src/main/java/oogasalad/engine/model/object/DefaultGameObject.java
+++ src/main/java/oogasalad/engine/model/object/DefaultGameObject.java
@@ -7,15 +7,18 @@ import oogasalad.engine.event.Event;
 import oogasalad.fileparser.records.SpriteData;
 
 /**
- * The player class represents a player object, which is typically controlled using key inputs (but not required to be)
- * Player's are typically how the user represents their own actions within the game
+ * The player class represents a player object, which is typically controlled using key inputs (but
+ * not required to be) Player's are typically how the user represents their own actions within the
+ * game
  *
  * @author Alana Zinkin
  */
 public class DefaultGameObject extends GameObject {
 
 
-  public DefaultGameObject(UUID uuid, int blueprintID, String type, int hitBoxX, int hitBoxY, int hitBoxWidth, int hitBoxHeight, int layer, String name, String group, SpriteData spriteData,
+  public DefaultGameObject(UUID uuid, int blueprintID, String type, int hitBoxX, int hitBoxY,
+      int hitBoxWidth, int hitBoxHeight, int layer, String name, String group,
+      SpriteData spriteData,
       Map<String, String> params, List<Event> events) {
     super(uuid, blueprintID, type, hitBoxX, hitBoxY, hitBoxWidth, hitBoxHeight, layer, name, group,
         spriteData, params, events);
