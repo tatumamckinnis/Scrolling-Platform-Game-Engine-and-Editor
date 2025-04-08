d92a2ea14dd0b73b008c88b8dd7d2faca8b4c676
Alana Zinkin <abz8@duke.edu>
2025-04-08 16:08:47 -0400

    Test: resolve broken default engine file test - relates to #194

--- src/test/java/oogasalad/engine/controller/DefaultEngineFileTest.java
+++ src/test/java/oogasalad/engine/controller/DefaultEngineFileTest.java
@@ -35,13 +35,9 @@ class DefaultEngineFileTest {
     gameObjectBluePrintData.add(gameObject1);
     Map<Integer, List<GameObjectData>> gameObjectsByLayer = new HashMap<>();
     gameObjectsByLayer.put(1, gameObjectBluePrintData);
-    //levelData = new LevelData("Super Mario Bros", bluePrintMap, gameObjectsByLayer);
+    levelData = new LevelData("Super Mario Bros", 500, 500, bluePrintMap, gameObjectsByLayer);
   }
 
-  @Test
-  void saveLevelStatus() {
-
-  }
 
   @Test
   void loadFileToEngine() {
