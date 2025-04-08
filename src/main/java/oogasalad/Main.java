e38f267a7f3150792627a58da6752eaf4c71c3f9
Alana Zinkin <abz8@duke.edu>
2025-04-08 15:59:23 -0400

    Doc: add main class javadoc

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -4,28 +4,38 @@ package oogasalad;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import oogasalad.engine.controller.DefaultGameManager;
-import oogasalad.engine.controller.DefaultLevel;
 import oogasalad.engine.controller.api.GameManagerAPI;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Feel free to completely  change this code or delete it entirely.
+ * Main class of application
  */
 public class Main extends Application {
-    private static final Logger LOG = LogManager.getLogger();
 
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            GameManagerAPI manager = new DefaultGameManager();
-            LOG.info("Starting game...");
-        } catch (Exception e) {
-            LOG.warn("Error starting main");
-        }
-    }
+  private static final Logger LOG = LogManager.getLogger();
 
-    public static void main(String[] args) {
-        launch(args); // Ensures JavaFX starts on the JavaFX Application Thread
+  /**
+   * @param primaryStage the primary stage for this application, onto which the application scene
+   *                     can be set. Applications may create other stages, if needed, but they will
+   *                     not be primary stages.
+   */
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      GameManagerAPI manager = new DefaultGameManager();
+      LOG.info("Starting game...");
+    } catch (Exception e) {
+      LOG.warn("Error starting main");
     }
+  }
+
+  /**
+   * main method of application
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    launch(args); // Ensures JavaFX starts on the JavaFX Application Thread
+  }
 }
