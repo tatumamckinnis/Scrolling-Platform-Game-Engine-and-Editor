834f3a6924a62758a40045966324d1d0c25f4f11
Billy McCune <wrm29@duke.edu>
2025-04-09 17:46:58 -0400

    Refactor Blueprints should match engine format: the BlueprintData record now better reflects our current design.

--- src/main/java/oogasalad/fileparser/records/BlueprintData.java
+++ src/main/java/oogasalad/fileparser/records/BlueprintData.java
@@ -5,11 +5,15 @@ import java.util.Map;
 
 public record BlueprintData(
     int blueprintId,
+    double velocityX,
+    double velocityY,
     String gameName,
     String group, //entities, blocks, backgrounds
     String type,
     SpriteData spriteData,
     HitBoxData hitBoxData,
     List<EventData> eventDataList,
-    Map<String,String> objectProperties
+    Map<String,String> stringProperties,
+    Map<String,Double> doubleProperties,
+    List<String> displayedProperties
 ) {}
