d3285e8f026f3a2d9568f11d041684d8e4e2fe01
Alana Zinkin <abz8@duke.edu>
2025-04-09 18:14:32 -0400

    Refactor: add javadoc comments to exception - relates to #192

--- src/main/java/oogasalad/exceptions/HitBoxParseException.java
+++ src/main/java/oogasalad/exceptions/HitBoxParseException.java
@@ -1,5 +1,8 @@
 package oogasalad.exceptions;
 
+/**
+ * Hit box parse exception thrown when the hitbox cannot be parsed
+ */
 public class HitBoxParseException extends Exception {
 
 }
--- src/main/java/oogasalad/exceptions/LayerParseException.java
+++ src/main/java/oogasalad/exceptions/LayerParseException.java
@@ -1,5 +1,8 @@
 package oogasalad.exceptions;
 
+/**
+ * Layer parse exception thrown when the layer cannot be parsed
+ */
 public class LayerParseException extends Exception {
 
 }
