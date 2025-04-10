efc8572afe711aa1b0aed51d02b811e92a1493c2
Alana Zinkin <abz8@duke.edu>
2025-04-09 23:39:05 -0400

    Test: resolve test errors

--- src/test/java/oogasalad/engine/controller/DefaultEngineFileTest.java
+++ src/test/java/oogasalad/engine/controller/DefaultEngineFileTest.java
@@ -32,18 +32,16 @@ class DefaultEngineFileTest {
   void setUp() {
     myEngineFile = new DefaultEngineFileConverter();
     Map<Integer, BlueprintData> bluePrintMap = new HashMap<>();
-    SpriteData spriteData1 = new SpriteData("Mario",
-        new FrameData("Mario Paused", 1, 1, 2, 4, new File("dd")), new ArrayList<>(),
+    SpriteData spriteData1 = new SpriteData("Mario", new File("src/test/resources/sprites1/sprite1.png"),
+        new FrameData("Mario Paused", 1, 1, 2, 4), new ArrayList<>(),
         new ArrayList<>());
     HitBoxData hitBoxData1 = new HitBoxData("Mario", 1, 1, 2, 4);
-    bluePrintMap.put(1, new BlueprintData(1, "Mario", "Player", "Player", spriteData1, hitBoxData1,
-        new ArrayList<>(), new HashMap<>()));
-    List<GameObjectData> gameObjectBluePrintData = new ArrayList<>();
+    bluePrintMap.put(1, new BlueprintData(1, 1, 1, "Mario", "Player", "Player", spriteData1, hitBoxData1,
+        new ArrayList<>(), new HashMap<>(), new HashMap<>(), new ArrayList<>()));
     GameObjectData gameObject1 = new GameObjectData(1, new UUID(4, 1), 1, 1, 0);
-    gameObjectBluePrintData.add(gameObject1);
-    Map<Integer, List<GameObjectData>> gameObjectsByLayer = new HashMap<>();
-    gameObjectsByLayer.put(1, gameObjectBluePrintData);
-    levelData = new LevelData("Super Mario Bros", 500, 500, bluePrintMap, gameObjectsByLayer);
+    List<GameObjectData> gameObjects = new ArrayList<>();
+    gameObjects.add(gameObject1);
+    levelData = new LevelData("Mario", 0, 500, 500, 0, bluePrintMap, gameObjects);
   }
 
 
@@ -58,7 +56,7 @@ class DefaultEngineFileTest {
     Map<String, AnimationData> animationMap = new HashMap<>();
 
     Sprite expectedSprite = new Sprite(frameMap, marioFrame, animationMap, 0, 0, new File("dd"));
-    HitBox expectedHitBox = new HitBox(10, 0, 2, 4); // x, y, width, height
+    HitBox expectedHitBox = new HitBox(1, 1, 2, 4); // x, y, width, height
 
     // Setup entity
     UUID uuid = new UUID(4, 1);
@@ -66,7 +64,7 @@ class DefaultEngineFileTest {
     Map<String, Double> doubleParams = new HashMap<>();
     List<Event> events = new ArrayList<>();
     expectedObjects.add(
-        new Entity(uuid, "Player", 1, 1.0, 5.0, expectedHitBox, expectedSprite, events,
+        new Entity(uuid, "Player", 1, 1.0, 1.0, expectedHitBox, expectedSprite, events,
             stringParams, doubleParams));
 
     // Call method under test
