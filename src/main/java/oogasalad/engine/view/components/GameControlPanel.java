a66b03ae7af11a41e29cb938875f4f425a607769
Luke Nam <lsn12@duke.edu>
2025-04-09 23:31:48 -0400

    Refactor DESIGN-15: Externalize configuration properties for game control panel

--- src/main/java/oogasalad/engine/view/components/GameControlPanel.java
+++ src/main/java/oogasalad/engine/view/components/GameControlPanel.java
@@ -1,7 +1,12 @@
 package oogasalad.engine.view.components;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,6 +16,8 @@ import oogasalad.engine.view.Display;
 import oogasalad.engine.view.ViewAPI;
 import oogasalad.engine.view.ViewState;
 import oogasalad.engine.view.factory.ButtonActionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * This class holds a panel of buttons such as a home button and a play/pause button.
@@ -18,15 +25,24 @@ import oogasalad.engine.view.factory.ButtonActionFactory;
  * @author Aksel Bell
  */
 public class GameControlPanel extends Display {
+  private static final Logger LOG = LogManager.getLogger();
   private List<Button> buttons;
   private String homeButtonID = "levelHomeButton";
   private ViewState viewState;
-  // TODO make it read from a property file the type of button, the image of the button
+
+  private static final String engineComponentPropertiesFilepath = "/oogasalad/screens/engineComponent.properties";
+  private static final Properties engineComponentProperties = new Properties();
 
   /**
    * Adds initial buttons to the control panel.
    */
   public GameControlPanel(ViewState viewState) {
+    try {
+      InputStream stream = getClass().getResourceAsStream(engineComponentPropertiesFilepath);
+      engineComponentProperties.load(stream);
+    } catch (IOException e) {
+      LOG.warn("Unable to load engine component properties");
+    }
     buttons = new ArrayList<>();
     this.viewState = viewState;
     createHomeButton();
@@ -40,21 +56,27 @@ public class GameControlPanel extends Display {
   public void render() {
     HBox buttonContainer = new HBox();
     buttonContainer.getChildren().addAll(buttons);
-    buttonContainer.setSpacing(10);
-    buttonContainer.setLayoutX(340);
+    int containerSpacing = Integer.parseInt(engineComponentProperties.getProperty("gameControlPanel.button.spacing"));
+    int containerLayoutX = Integer.parseInt(engineComponentProperties.getProperty("gameControlPanel.button.layoutX"));
+    buttonContainer.setSpacing(containerSpacing);
+    buttonContainer.setLayoutX(containerLayoutX);
 
     this.getChildren().add(buttonContainer);
   }
 
   private void createHomeButton() {
-    Image image = new Image("oogasalad/gameIcons/home.jpeg");
+    Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(
+            engineComponentProperties.getProperty("gameControlPanel.image.home"))));
     ImageView imageView = new ImageView(image);
-    imageView.setFitWidth(40);
-    imageView.setFitHeight(40);
 
+    int homeFitWidth = Integer.parseInt(engineComponentProperties.getProperty("gameControlPanel.image.home.width"));
+    int homeFitHeight = Integer.parseInt(engineComponentProperties.getProperty("gameControlPanel.image.home.height"));
+
+    imageView.setFitWidth(homeFitWidth);
+    imageView.setFitHeight(homeFitHeight);
     Button homeButton = new Button();
     homeButton.setGraphic(imageView);
-    homeButton.setMinSize(40, 40);
+    homeButton.setMinSize(homeFitWidth, homeFitHeight);
     ButtonActionFactory factory = new ButtonActionFactory(viewState);
     homeButton.setOnAction(event -> {
       factory.getAction(homeButtonID).run();
