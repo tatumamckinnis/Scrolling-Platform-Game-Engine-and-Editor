22957bd73d0d0068938f958fdcde4982bec33f49
Luke Nam <lsn12@duke.edu>
2025-04-15 22:47:13 -0400

    Feature OOGA-45: Remove the play button from NewGameComponents in favor of the play button in GameControlPanel

--- src/main/java/oogasalad/engine/view/components/NewGameComponents.java
+++ src/main/java/oogasalad/engine/view/components/NewGameComponents.java
@@ -1,34 +1,36 @@
 package oogasalad.engine.view.components;
 
-import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import oogasalad.engine.view.Display;
 import oogasalad.engine.view.ViewState;
 
+/**
+ * This class hold the components for a new game screen once the player either completes a level or
+ * loses a life. It displays the high score of the player currently.
+ * @author Aksel Bell, Luke Nam
+ */
 public class NewGameComponents extends Display {
-
-  // contains a game over object if it is game ober
-  // contains a play again
-  // contains a high score object
   private final ViewState viewState;
   private final Text highScore;
-  private final Button play;
 
+  /**
+   * Constructor for the NewGameComponents class that initializes the high score text.
+   * TODO: Externalize high score configuration
+   * @param viewState the current view state of the game
+   */
   public NewGameComponents(ViewState viewState) {
     this.viewState = viewState;
     highScore = new Text("High score: " + 0);
-    play = new Button("Play");
-    play.setOnAction(event -> {
-      viewState.getGameManager().playGame();
-      this.hide();
-    });
   }
 
+  /**
+   * Renders the high score text on the game engine screen.
+   */
   @Override
   public void initialRender() {
     VBox buttonContainer = new VBox();
-    buttonContainer.getChildren().addAll(highScore, play);
+    buttonContainer.getChildren().add(highScore);
     buttonContainer.setSpacing(20);
     buttonContainer.setLayoutX(250); // change to levelViewWidth / 2
 
