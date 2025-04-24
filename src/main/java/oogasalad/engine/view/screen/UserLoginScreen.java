970c3311595b0c057e69e604fb4e338a91357a6f
Billy McCune <wrm29@duke.edu>
2025-04-24 17:54:05 -0400

    Refactor User Data view: added a user login screen and session managment

---
+++ src/main/java/oogasalad/engine/view/screen/UserLoginScreen.java
@@ -0,0 +1,17 @@
+package oogasalad.engine.view.screen;
+
+import oogasalad.engine.model.object.ImmutableGameObject;
+import oogasalad.engine.view.Display;
+
+public class UserLoginScreen extends Display {
+
+  @Override
+  public void removeGameObjectImage(ImmutableGameObject gameObject) {
+
+  }
+
+  @Override
+  public void renderPlayerStats(ImmutableGameObject player) {
+
+  }
+}
